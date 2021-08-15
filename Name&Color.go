adf8020019b81d6c627b93d0b8cea366394584c0
garciamilord <garciamilord@gmail.com>
2021-08-15 00:01:52 -0400

    "add exception five color and recursive function"

--- Name&Color.go
+++ Name&Color.go
@@ -1,17 +1,31 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main() {
-
+func red() {
 	name, color := "", ""
 
 	fmt.Printf("\nEnter a name and ")
 	fmt.Printf("your favorite color: ")
 	fmt.Scan(&name)
 	fmt.Scan(&color)
+	if color == "blue" {
+		fmt.Printf("My name is %s and my favorite color %s", name, color)
+	} else if color == "red" {
+		fmt.Printf("My name is %s and my favorite color %s", name, color)
+	} else if color == "yellow" {
+		fmt.Printf("My name is %s and my favorite color %s", name, color)
+	} else if color == "green" {
+		fmt.Printf("My name is %s and my favorite color %s", name, color)
+	} else {
+		fmt.Printf("wrong answer")
+		red()
 
-	fmt.Printf("My name is %s and my favorite color %s", name, color)
+	}
+
+}
+
+func main() {
+	red()
+
 }
